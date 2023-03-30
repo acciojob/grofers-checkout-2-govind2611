a1cbaf7b62cd55f822f8045072408c86abe24ffc
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-03-30 10:08:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+ // Get all the price elements in the table
+  const priceElements = document.querySelectorAll('[data-ns-test="prices"]');
+
+  // Initialize a variable to store the total price
+  let totalPrice = 0;
+
+  // Loop through each price element and add its value to the total price
+  priceElements.forEach(priceElement => {
+    totalPrice += parseFloat(priceElement.textContent);
+  });
+
+  // Set the grand total element to the total price
+  const grandTotalElement = document.querySelector('[data-ns-test="grandTotal"]');
+  grandTotalElement.textContent = totalPrice.toFixed(2);
